2fad5f8a9cede83dd00f0d9fd848708686220086
nikita <nikitosan1717@gmail.com>
2021-09-20 00:43:53 +0300

    schedule started

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('contacts/', views.contacts_page, name='contacts'),
     path('', views.homepage, name='homepage'),
+    path('schedule/', views.schedule_page, name='schedule')
 ]
--- main/views.py
+++ main/views.py
@@ -7,6 +7,9 @@ from posts.models import Post
 def contacts_page(request):
     return render(request, 'contacts.html')
 
+def schedule_page(request):
+    return render(request, 'main/schedule.html')
+
 def homepage(request):
     context = {
         'posts': Post.objects.all()
