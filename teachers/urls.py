de039605e590e86f28dffbf54c0cb723b1f31407
nikita <nikitosan1717@gmail.com>
2021-09-29 00:52:23 +0300

    one more update

--- teachers/urls.py
+++ teachers/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teachers/', views.teacher_page, name='teachers'),
-    path('teachers/<slug:slug>/', views.teacher_detail, name='detail'),
-    
-
+    path('teachers/', views.teacher_page, name='teachers')
 ]
